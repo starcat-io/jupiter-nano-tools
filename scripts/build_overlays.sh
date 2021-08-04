02ab3921048c539212c544fe551ff1b1fe0ef17e
Adam Feuer <adam@starcat.io>
2021-08-04 14:26:18 -0700

    renamed overlays to JN

--- scripts/build_overlays.sh
+++ scripts/build_overlays.sh
@@ -9,22 +9,22 @@ overlays_dir="${output_dir}/overlays"
 mkdir -p "${overlays_dir}"
 
 # overlays array
-declare -a overlays=("GB-24LCD-FEATHERWING" 
-					"GB-35LCD-FEATHERWING"
-					"GB-ADC"
-					"GB-ETHERNET-FEATHERWING"
-					"GB-I2C0"
-					"GB-I2S0"
-					"GB-I2S0-NO-MCK"
-					"GB-I2S0-NO-MCK-NO-DI"
-					"GB-PWM1"
-					"GB-PWM1-3"
-					"GB-PWM2"
-					"GB-SPI0-ENC28J60"
-					"GB-SPI0-SPIDEV-CS-PWML1"
-					"GB-SPI0-SPIDEV-NO-CS"
-					"GB-WIFI-FEATHERWING"
-					"GB-UART2-FLX4-AD2-AD3")
+declare -a overlays=("JN-24LCD-FEATHERWING" 
+					"JN-35LCD-FEATHERWING"
+					"JN-ADC"
+					"JN-ETHERNET-FEATHERWING"
+					"JN-I2C0"
+					"JN-I2S0"
+					"JN-I2S0-NO-MCK"
+					"JN-I2S0-NO-MCK-NO-DI"
+					"JN-PWM1"
+					"JN-PWM1-3"
+					"JN-PWM2"
+					"JN-SPI0-ENC28J60"
+					"JN-SPI0-SPIDEV-CS-PWML1"
+					"JN-SPI0-SPIDEV-NO-CS"
+					"JN-WIFI-FEATHERWING"
+					"JN-UART2-FLX4-AD2-AD3")
 
 # Install all the libs listed in the blinka_libs array
 #dtc -@ -I dts -O dtb -o ${overlays_dir}/"$i".dtbo ${patch_dir}/"$i".dts
