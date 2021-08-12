19c827618415434bebf716085385e7ff2452b205
Adam Feuer <adam@starcat.io>
2021-08-12 16:41:01 -0700

    switch back to debian stretch (9.x)

--- scripts/chroot_min.sh
+++ scripts/chroot_min.sh
@@ -53,11 +53,11 @@ add_user () {
 
 # Add sources
 cat <<EOT > /etc/apt/sources.list
-deb http://deb.debian.org/debian buster main contrib non-free
-deb-src http://deb.debian.org/debian buster main contrib non-free
+deb http://deb.debian.org/debian stretch main contrib non-free
+deb-src http://deb.debian.org/debian stretch main contrib non-free
 
-deb http://security.debian.org/debian-security buster/updates main contrib
-deb-src http://security.debian.org/debian-security buster/updates main contrib
+deb http://security.debian.org/debian-security stretch/updates main contrib
+deb-src http://security.debian.org/debian-security stretch/updates main contrib
 EOT
 
 # Update sources
