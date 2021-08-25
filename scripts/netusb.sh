f08e5965c4ca3313110b25d142fc1cb8b4eb5d29
Adam Feuer <adam@starcat.io>
2021-08-25 16:55:49 -0700

    improved error handling with routes and iptables
    - also added show command to show USB Ethernet Gadget interface

--- scripts/netusb.sh
+++ scripts/netusb.sh
@@ -29,23 +29,31 @@ IP_BROADCAST="10.0.0.255"
 IP_HOST="10.0.0.1"
 IP_NUTTX="10.0.0.2"
 
-if [ $# != 3 ]; then
-  echo "Usage: $0 <main-interface> <usb-net-interface> <on|off>"
-  exit 1
-fi
-
 IF_HOST=$1
 IF_USB=$2
 STATUS=$3
 
+if [ "$1" != "show" ] && [ $# -ne 3 ]; then
+  echo "Usage: $0 <main-interface> <usb-net-interface> <on|off>"
+  echo " or to show USB Ethernet gadget interface:"
+  echo "       $0 show" 
+  exit 1
+fi
+
+find_interface() {
+    interface="$(ip -o link | egrep '(enx|usb)' | awk '{ print $2 }'| sed 's/://g')"
+    echo $interface
+}
+
 net_off() {
-  ip route delete $IP_NET
-  ip route delete $IP_NUTTX/32
+  # delete routes if necessary
+  [ $(ip route show $IP_NET | wc -l) -eq 0 ] || ip route delete $IP_NET 
+  [ $(ip route show $IP_NUTTX/32 | wc -l) -eq 0 ] || ip route delete $IP_NUTTX/32
 
   # delete nat rules to clean up
-  iptables -t nat -D POSTROUTING -o $IF_HOST -j MASQUERADE
-  iptables -D FORWARD -i $IF_HOST -o $IF_USB -m state --state RELATED,ESTABLISHED -j ACCEPT
-  iptables -D FORWARD -i $IF_USB -o $IF_HOST -j ACCEPT
+  iptables --table nat -C POSTROUTING -o $IF_HOST -j MASQUERADE || iptables --table nat -D POSTROUTING -o $IF_HOST -j MASQUERADE  
+  iptables -C FORWARD -i $IF_HOST -o $IF_USB -m state --state RELATED,ESTABLISHED -j ACCEPT || iptables -D FORWARD -i $IF_HOST -o $IF_USB -m state --state RELATED,ESTABLISHED -j ACCEPT
+  iptables -C FORWARD -i $IF_USB -o $IF_HOST -j ACCEPT || iptables -D FORWARD -i $IF_USB -o $IF_HOST -j ACCEPT
 
   ip route show
   ifconfig $IF_USB down
@@ -54,7 +62,9 @@ net_off() {
 if [ "$STATUS" == "on" ]; then
   net_off
   ifconfig $IF_USB up
-  ifconfig -a
+  echo
+  ifconfig $IF_USB
+  echo
   #ifconfig $IF_USB add $IP_HOST
   ifconfig $IF_USB $IP_HOST
   #ifconfig $IF_USB:0 broadcast $IP_BROADCAST netmask $IP_NETMASK
@@ -68,6 +78,12 @@ if [ "$STATUS" == "on" ]; then
   iptables -A FORWARD -i $IF_USB -o $IF_HOST -j ACCEPT
 
   ip route show
-else
+  echo
+  ping -c 1 10.0.0.2
+elif [ "$STATUS" == "off" ]; then
   net_off
+elif [ "$IF_HOST" == "show" ]; then
+  # show interface  
+  ifconfig "$(find_interface)"
 fi
+echo
